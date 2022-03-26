4c54999078429bd2d533b3d1b01254f5dbdde16e
Siamak.Ahmadi <mrsiamak.dev@gmail.com>
2022-03-26 18:57:44 +0430

    2

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Nxdext.js!</a>
+          Welcome to <a href="https://nextjs.org">Nxdedxt.js!</a>
         </h1>
 
         <p className={styles.description}>
