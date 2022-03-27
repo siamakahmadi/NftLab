3195ee513aa08db0c1b7a5b9ac4e36fd903c28ed
Siamak.Ahmadi <mrsiamak.dev@gmail.com>
2022-03-27 21:22:32 +0430

    Remove Flash Icon

--- pages/Components/Card/index.js
+++ pages/Components/Card/index.js
@@ -1,21 +1,16 @@
 import style from './style.module.scss'
 import Button from '../Button/index'
 
+
 export default function index() {
+
   return (
     <div className={style.container}>
       <div className={style.navigation}>
         <div>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M9.57 5.92993L3.5 11.9999L9.57 18.0699" stroke="#FCF3FD" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M20.5019 12H3.67188" stroke="#FCF3FD" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
         </div>
         <div>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M14.4297 5.92993L20.4997 11.9999L14.4297 18.0699" stroke="#FCF3FD" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M3.5 12H20.33" stroke="#FCF3FD" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
+
         </div>
       </div>
       <div className={style.cardList}>
